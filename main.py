8c0dabad1e93f51d8eb42f7f9da164da8a0a21c7
hng011 <hansnaufal.3040@gmail.com>
2025-04-18 20:22:56 +0700

    fix modules

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-from modules.TextGeneration import text_generation_page 
+from modules.Pages import text_generation_page 
 
 
 def main():
