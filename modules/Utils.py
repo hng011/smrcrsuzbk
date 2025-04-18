8d76bf4c7e467f010747c88deab8fd4a1f10a784
hng011 <hansnaufal.3040@gmail.com>
2025-04-18 19:15:30 +0700

    fix Utils

--- modules/Utils.py
+++ modules/Utils.py
@@ -3,7 +3,7 @@ from .__init__ import *
 def call_openrouter(system_prompt, user_prompt, model="qwen/qwq-32b:free"):        
         try:
             response = requests.post(
-                url=os.getenv("OPENROUTER_ENDPOINT"),
+                url=OPENROUTER_ENDPOINT,
                 headers={
                     "Authorization": f"Bearer {OPENROUTER_API_KEY}",
                     "Content-Type": "application/json",
